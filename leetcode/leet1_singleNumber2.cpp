bdf6bbcaec148aeabe1445f97f92e6d279ca23c5
klambert <kevleyski@gmail.com>
2020-04-06 19:22:14 +1000

    leetcode week 1 problem 1

--- leetcode/leet1_singleNumber2.cpp
+++ leetcode/leet1_singleNumber2.cpp
@@ -1,48 +1,48 @@
 #include <iostream>
 
+/* OPTIMIZATION
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4
+ */
+
 int singleNumber2 (int *nums, int numsSize)
 {
-  // assert nums and numSize > 0
+    // assert nums and numSize > 0
 
-  int *numsSentinel = nums + numsSize;
-  int *numsPtr = nums;
-  int singleNum = 0;
+    int *numsSentinel = nums + numsSize;
+    int *numsPtr = nums;
+    int singleNum = 0;
 
-  // Iterate to see which number minus the others is 0, if none ar then that your number
-  while (numsPtr < numsSentinel) {
-    int *numsPtrTest = nums;
-    int matchFound = 0;
-    while (numsPtrTest < numsSentinel) {
-      if (numsPtrTest != numsPtr) {
-        if ((*numsPtr - *numsPtrTest) == 0) {
-          // Match found so cant be this one
-          matchFound = 1;
-          break;
-        }
-      }
-      numsPtrTest++;
+    // Iterate to see which number minus the others is 0, if none ar then that your number
+    while (numsPtr < numsSentinel) {
+        singleNum ^= *numsPtr;
+        numsPtr++;
     }
-    if (!matchFound)
-      singleNum = *numsPtr;
 
-    numsPtr++;
-  }
-
-  return singleNum;
+    return singleNum;
 }
 
 int leet1_test2() {
-  int foo[3] = {2, 2, 1};
+    int foo[3] = {2, 2, 1};
+    std::cout << "{2, 2, 1}=" << singleNumber2(foo, 3) << std::endl;
 
-  std::cout << "{2, 2, 1}=" << singleNumber2(foo, 3) << std::endl;
+    int foo2[5] = {4,1,2,1,2};
+    std::cout << "{4,1,2,1,2}=" << singleNumber2(foo2, 5) << std::endl;
 
-  int foo2[5] = {4,1,2,1,2};
+    int foo3[5] = {1,3,1,-1,3};
+    std::cout << "{1,3,1,-1,3}=" << singleNumber2(foo3, 5) << std::endl;
 
-  std::cout << "{4,1,2,1,2}=" << singleNumber2(foo2, 5) << std::endl;
-
-  int foo3[5] = {1,3,1,-1,3};
-
-  std::cout << "{1,3,1,-1,3}=" << singleNumber2(foo3, 5) << std::endl;
-
-  return 0;
+    return 0;
 }
--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+using namespace std;
 
 extern int leet1_test();
 extern int leet1_test2();
 
 int main() {
+    int timer = clock();
     //leet1_test();
     leet1_test2();
 
-  return 0;
+    cout << endl << "time=" << clock() - timer << endl;
+
+    return 0;
 }
